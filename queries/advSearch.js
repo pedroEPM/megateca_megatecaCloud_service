0bc0af0940bac91bed6111f85f37fca22204740c
pedroEPM <110569970+pedroEPM@users.noreply.github.com>
2024-01-25 16:40:01 -0600

    added console.log()

--- queries/advSearch.js
+++ queries/advSearch.js
@@ -13,6 +13,8 @@ const schemaType = (type) => {
 class AdvSearch {
     async getAllInfo(type, settings, settingsForCounter, firstTimeSearch) {
         try {
+
+            console.log(settingsForCounter)
             const db = await openDB();
             const schemaName = schemaType(type);
             let getAll = [];
